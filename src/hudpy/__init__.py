8f27490e3c9ada395883f6c9d21ec94913903bc1
Emmet Tam <etam4269@emmets-air.mynetworksettings.com>
2022-06-13 00:04:56 -0400

    Fixing classifider in setup.cfg file. Making sure import from * imports all modules from hudpy. Removed unused file hud_misc_helpers.

---
+++ src/hudpy/__init__.py
@@ -0,0 +1,10 @@
+__all__ = ["hud_cache", "hud_chas", "hud_cw",
+           "hud_dataset_cw", "hud_do_query_calls",
+           "hud_download_bar", "hud_fmr",
+           "hud_geo_in_geo", "hud_get_recent_data",
+           "hud_input_check", "hud_internet_online",
+           "hud_key",
+           "hud_misc", "hud_pkg_env",
+           "hud_user_agent", "hud_user",
+           "hud_website_docs"
+          ]
